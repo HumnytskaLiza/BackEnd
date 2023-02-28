65fe34b2d4322288d245b1f9a7734c90c312174a
HumnytskaLiza <gumnickajaliza@gmail.com>
2023-02-28 09:03:01 +0200

    update

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const Mongo = require("./setup/mongoose");
+
+require("dotenv").config();
+
+const { usersApiRouter } = require("./api/usersAPI");
+const { sessionsApiRouter } = require("./api/sessionsAPI");
+const { theatersApiRouter } = require("./api/theatersAPI");
 
 const app = express();
-const port = 8080;
+app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  res.status(200).send("Hello World!");
-});
+const setup = async () => {
+  await Mongo.setupDb(process.env.MONGO_DB_URI);
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+  // fisrt API route
+  app.use(sessionsApiRouter);
+
+  // second and third API routes
+  app.use(theatersApiRouter);
+
+  // fourth API route
+  app.use(usersApiRouter);
+
+  app.listen(process.env.PORT, () => {
+    console.log(`Server started on port ${process.env.PORT}`);
+  });
+};
+
+setup();
